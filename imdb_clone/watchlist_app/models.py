7e47705601f0e3c619e72e544f69653a54167bf1
hamza-wajeeh <hamza.wajeeh@inseyab.com>
2024-03-24 23:37:48 +0500

    added movies model

--- imdb_clone/watchlist_app/models.py
+++ imdb_clone/watchlist_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255, null=True, blank=True),
+    title = models.CharField(max_length=155, null=True, blank=True)
     description = models.TextField(blank=True, null=True, max_length=255)
     active = models.BooleanField(default=True)
 
