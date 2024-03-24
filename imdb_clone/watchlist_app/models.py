625488c5dcab95a763ede38656d36f2deffce752
hamza-wajeeh <hamza.wajeeh@inseyab.com>
2024-03-24 23:31:01 +0500

    Initial

---
+++ imdb_clone/watchlist_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True),
+    description = models.TextField(blank=True, null=True, max_length=255)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
