d1f80af017fae861ea03ab80277d9c47a3d76d68
hamza-wajeeh <hamza.wajeeh@inseyab.com>
2024-03-25 14:08:52 +0500

    Status Codes

--- imdb_clone/watchlist_app/api/views.py
+++ imdb_clone/watchlist_app/api/views.py
@@ -1,18 +1,62 @@
 from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import MovieSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def movie_list(request):
 
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
 
-@api_view(['GET'])
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE', 'PATCH'])
 def movie_detail(request, id):
 
-    movie = Movie.objects.get(pk=id)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == 'GET':
+
+        try:
+            movie = Movie.objects.get(pk=id)
+            serializer = MovieSerializer(movie)
+            return Response(serializer.data)
+        except Movie.DoesNotExist:
+            return Response("Movie not found", status=status.HTTP_404_NOT_FOUND)
+
+
+    elif request.method == 'PUT':
+
+        try:
+            movie = Movie.objects.get(pk=id)
+        except Movie.DoesNotExist:
+            return Response("Movie not found", status=status.HTTP_404_NOT_FOUND)
+
+        serializer = MovieSerializer(movie, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    elif request.method == 'PATCH':
+        pass
+
+
+    elif request.method == 'DELETE':
+
+        try:
+            movie = Movie.objects.get(pk=id)
+            movie.delete()
+            return Response("Movie deleted succesfully", status=status.HTTP_204_NO_CONTENT)
+        except Movie.DoesNotExist:
+            return Response("Movie not found", status=status.HTTP_404_NOT_FOUND)
